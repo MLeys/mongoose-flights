84cec595ac581ecc8badc04ff031e6843e970018
username <mleyspainting@gmail.com>
2022-11-30 14:42:16 -0500

    Copied over styling. Work on Lab 2 and default date

--- controllers/flights.js
+++ controllers/flights.js
@@ -7,7 +7,26 @@ module.exports = {
 }
 
 function create(req, res) {
-    console.log(req.body)
+
+    Flight.create(req.body, function(err, flightDoc){
+
+        if (err) {
+            console.log(err);
+        }
+        console.log(flightDoc);
+
+        res.redirect('/flights');
+    })
+    // console.log(req.body, ' <---------------------------')
+    // const newFlight = new Flight();
+    // // Obtain the default date
+    // const dt = newFlight.departs;
+    // console.log(dt, " DT DFGFASDFDSFDSAASFDSSADSDFSA")
+    // // Format the date for the value attribute of the input
+    // const departsDate = dt.toISOString().slice(0, 16);
+    // console.log('==================================')
+    // console.log(departsDate);
+    
 }
 
 function index(req, res) {
@@ -15,7 +34,7 @@ function index(req, res) {
 
         console.log(flightDocs)
 
-        res.render('flights/', {flights: flightDocs}); 
+        res.render('flights/index', {flights: flightDocs}); 
         // injecting the movies variable to use in pages ejs file
     })
 }
@@ -27,6 +46,7 @@ function newFlight(req, res) {
     // // Format the date for the value attribute of the input
     // const departsDate = dt.toISOString().slice(0, 16);
     // res.render('flights/new', {departsDate});
+    // Flight.findById(req.params.id, )
 
 
     res.render('flights/new');
--- models/flight.js
+++ models/flight.js
@@ -4,25 +4,18 @@ const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
     airline: { type: String, enum: ['American', 'Delta', 'Southwest', 'United'] },
-    airport: {
-        type: String,
-        default: 'DEN',
-        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN']
-    },
-    flightNo: {
-        type: Number,
-        min: 10,
-        max: 9999,    
-    },
+    airport: { type: String, default: 'DEN', enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'] },
+    flightNo: {type: Number, min: 10, max: 9999, required: 'Must have know flight number to submit' },
     departs: {
         type: Date,
         default: function() {
             const today = new Date();
-            Date.setFullYear(date.getFullYear() + 1);
-            return date;
-        },
+            const todayPlusOneYear =today.setFullYear(date.getFullYear() + 1);
+            console.log(todayPlusOneYear, " <--------- NEW DATE=============");
+            todayPlusOneYear = todayPlusOneYear.toISOString().slice(0,16)
+            return todayPlusOneYear;
+        }
     }
-
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
